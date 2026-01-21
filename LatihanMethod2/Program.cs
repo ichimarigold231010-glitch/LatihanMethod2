cb78b607611ea962184ccdc0a86deec0950d8f52
ichimarigold231010-glitch <ichimarigold231010@gmail.com>
2026-01-21 15:09:13 +0700

    hitung diskon dan hitung jumlah depaarttemen store

--- LatihanMethod2/Program.cs
+++ LatihanMethod2/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿int hargaItem, jumlahItem;
+
+decimal diskon, totalBayar;
+
+decimal hitungDiskon(int jml_item)
+{
+    if (jml_item == 5)
+        return 50000;
+    else if (jml_item == 10)
+        return 75000;
+    else
+        return 0;
+}
+
+decimal hitungTotalBayar(int jml_item, int harga_item, decimal diskon)
+{
+    return jml_item * harga_item - diskon;
+
+}
+
+Console.Write("Masukkan jumlah item: ");
+jumlahItem = int.Parse(Console.ReadLine());
+
+Console.Write("Masukkan harga item: Rp ");
+hargaItem = int.Parse(Console.ReadLine());
+
+diskon = hitungDiskon(jumlahItem);
+
+totalBayar = hitungTotalBayar(jumlahItem, hargaItem, diskon);
+
+Console.WriteLine($"Total bayar: Rp {totalBayar}");
